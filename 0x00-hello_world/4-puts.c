48afa9aa03dac2d1aca3028160c6181248f89bbf
Reem-Elsherif <Reemshkeep6@gmail.com>
2024-01-08 13:06:55 +0200

    printing using puts and betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main -Entry Point
+ *
+ * Description: A C Program prints statment weithout vari using puts func.
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
