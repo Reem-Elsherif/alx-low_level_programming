d22f2667b1a7d8af31744e349c482eb0b28923ea
Reem-Elsherif <Reemshkeep6@gmail.com>
2024-01-08 14:09:53 +0200

    adding the type float

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main Entry - point
+ * main - Entry point
  *
  * Description: printf(\"Size of a %lu byte(s)\n\",sizeof(the type of char));
  *
@@ -11,9 +11,10 @@
 int main(void)
 {
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
-	printf("Size of a long long int:%lu byte(s)\n", (unsigned long)sizeof(long long int));
+	printf("Size of a char: %u byte(s)\n", (unsigned long)sizeof(char));
+	printf("Size of an int: %u byte(s)\n", (unsigned long)sizeof(int));
+	printf("Size of a long int: %u byte(s)\n", (unsigned long)sizeof(long int));
+	printf("Size of a long long int:%u byte(s)\n", (unsigned long)sizeof(long long int));
+	printf("Size of a float: %u byte(s)\n", (unsigned long)sizeof(float));
 	return (0);
 }
